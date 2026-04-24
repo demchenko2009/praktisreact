64fb526b52a5f58ae71cfc38d37ac50328c4947e
demchenko2009 <166243357+demchenko2009@users.noreply.github.com>
2026-04-25 00:27:00 +0200

    gdgdfgdfgdgdgdg

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css'
+// import BlogersItem from './components/Blogers/Blogers'
+// import blog from "./blog.json"
+
+// function App() {
+//   return (
+//     <>
+//       {blog.map((fotoman) => (
+//         <BlogersItem key={fotoman.id} {...fotoman} />
+//       ))}
+//     </>
+//   )
+// }
+
+// export default App
---
+++ src/components/Blogers/Blogers.jsx
@@ -0,0 +1,25 @@
+// function BlogersItem({
+//   id,
+//   name,
+//   realName,
+//   platform,
+//   subscribers,
+//   category,
+//   country,
+//   age,
+//   avatar
+// }) {
+//   return (
+//     <Item key={id}>
+//       <Titles>{name}</Titles>
+//       <Subtitle>{realName}</Subtitle>
+//       <Folow>{platform}</Folow>
+//       <Categories>{category}</Categories>
+//       <Lands>{country}</Lands>
+//       <Older>{age}</Older>
+//       <Foto src={avatar} alt={name} />
+//     </Item>
+//   )
+// }
+
+// export default BlogersItem
